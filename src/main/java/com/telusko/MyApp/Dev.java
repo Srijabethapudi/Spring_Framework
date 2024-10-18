b7d870285dddc814f0a7e71ace42568b34cf3ea9
Srija <srijabethapudi@gmail.com>
2024-10-18 12:34:40 +0530

    myapp

---
+++ src/main/java/com/telusko/MyApp/Dev.java
@@ -0,0 +1,12 @@
+package com.telusko.MyApp;
+
+import org.springframework.stereotype.Component;
+
+@Component     //component annotation job is to create object
+public class Dev
+{
+    public void build()
+    {
+        System.out.println("Working on awesome project");
+    }
+}
